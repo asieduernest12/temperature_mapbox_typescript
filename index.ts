f9426bf8170d54047b61f6f45244d5085d67ec5a
linuxdev <asieduernest@yahoo.co.uk>
2022-11-27 06:17:14 -0500

    Fix: server, receive upload, save data to in memory db, return data

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
-import dotenv from "dotenv";
-import express, { Express, Request, Response } from "express";
-import path from "path";
-import cors from "cors";
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+import path from 'path';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+const multer = require('multer');
+const upload = multer(); // v1.0.5/ for parsing multipart/form-data
 
 const dataInMemory: {}[] = [];
 
@@ -9,36 +12,37 @@ dotenv.config();
 
 const app: Express = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("<h1>Hello World From the Typescript Server!</h1>");
+// app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('<h1>Hello World From the Typescript Server!</h1>');
+});
+
+app.get('/test', (req, res) => res.send('working'));
+
+app.post('/upload-file', upload.array(), (req, res) => {
+	try {
+		// const fileContents = JSON.parse(req.files.exampleData.data.toString('utf8'));
+		const { file = [] } = req.body;
+		const fileJson = JSON.parse(file);
+		dataInMemory.push(...fileJson);
+		return res.json({ contents: fileJson.length });
+	} catch (error) {
+		console.trace(error);
+		res.status(500).send(error);
+	}
+});
+
+app.get('/points', (req, res) => {
+	return res.json(dataInMemory);
 });
 
 const port = process.env.PORT || 8000;
 
-app.post("/upload-file", (req: Request, res: Response) => {
-  // const formData: FileInputs = req.body;
-
-  // console.log(formData);
-
-  // if (!formData) {
-  //   return res.status(404).send("File Not Found!");
-  // }
-
-  // return res.status(200).json(formData);
-
-  //read file
-  // add fil entris to dataInMemory
-  //jsofile contents is json
-  // const fileContents = req.body
-  const fileContents = JSON.parse(req.files.exampleData.data.toString("utf8"));
-  console.log({ fileContents });
-  dataInMemory.append(JSON.parse(fileContents));
-  return res.send("");
-});
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
